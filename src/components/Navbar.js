4b69517a7a1f805f9d0997a7199e423b0ca749bc
msksaul <64514098+msksaul@users.noreply.github.com>
2020-09-29 18:18:18 -0600

    Changes Deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,13 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,
-  NavbarText
+  DropdownItem
 } from 'reactstrap';
 
 function NavBar() {
